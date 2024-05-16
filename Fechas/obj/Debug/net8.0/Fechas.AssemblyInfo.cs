76864f05730bfa8452bf7c5f43c4eae81f25465a
ceso217 <baronicecilio@gmail.com>
2024-05-16 04:46:00 -0300

    Fechas

--- Fechas/obj/Debug/net8.0/Fechas.AssemblyInfo.cs
+++ Fechas/obj/Debug/net8.0/Fechas.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Fechas")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de4e90e96470a3b3b2055b73d4b8be1f4e1ace60")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b9cab82e961bbbd51a4373bd84852f1faff0196")]
 [assembly: System.Reflection.AssemblyProductAttribute("Fechas")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Fechas")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
