6d1122bc38555b61aabd8ca677256fd088439896
ceso217 <baronicecilio@gmail.com>
2024-06-13 10:54:20 -0300

    Ejercicio de archivos winforms

---
+++ ArchivosDeUnaCarpeta/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace ArchivosDeUnaCarpeta
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxResults = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxResults
+            // 
+            this.textBoxResults.Location = new System.Drawing.Point(12, 12);
+            this.textBoxResults.Multiline = true;
+            this.textBoxResults.Name = "textBoxResults";
+            this.textBoxResults.ReadOnly = true;
+            this.textBoxResults.Size = new System.Drawing.Size(776, 358);
+            this.textBoxResults.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(127, 356);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(125, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Elegir Directorio";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(775, 329);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(323, 356);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(125, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Guardar Datos";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(513, 356);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(125, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Cargar Datos";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(799, 391);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxResults;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ ArchivosDeUnaCarpeta/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ArchivosDeUnaCarpeta
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            float tamanioDirectorio = 0.0f;
+            FolderBrowserDialog elegirDirectorio = new FolderBrowserDialog();
+            elegirDirectorio.ShowDialog();
+
+            string path = elegirDirectorio.SelectedPath;
+
+            DirectoryInfo directorio = new DirectoryInfo(path);
+            FileSystemInfo[] contenido = directorio.GetFileSystemInfos();
+            List<FileInfo> archivos = new List<FileInfo> { };
+            List<DirectoryInfo> subcarpetas = new List<DirectoryInfo>();
+
+            foreach (FileSystemInfo elemento in contenido)
+            {
+                if (elemento is FileInfo)
+                {
+                    archivos.Add((FileInfo)elemento);
+                }
+            }
+
+            foreach (FileSystemInfo elemento in contenido)
+            {
+                if (elemento is DirectoryInfo)
+                {
+                    subcarpetas.Add((DirectoryInfo)elemento);
+                }
+            }
+
+            foreach (FileInfo archivo in archivos)
+            {
+                tamanioDirectorio += archivo.Length;
+            }
+
+            textBox1.Text = $"Archivos del directorio {directorio.Name} Tamaño de directorio: {tamanioDirectorio} bytes Ubicación: {path}\r\n";
+
+
+            foreach (FileInfo archivo in archivos)
+            {
+                textBox1.Text += $"Nombre: {archivo.Name} Tamaño: {archivo.Length} bytes Tamaño relativo al directorio: {(archivo.Length / tamanioDirectorio) * 100}%  \r\n";
+            }
+
+            textBox1.Text += $"\r\nSubcarpetas del directorio {directorio.Name}\r\n";
+
+            foreach (DirectoryInfo subcarpeta in subcarpetas)
+            {
+                if (subcarpeta is DirectoryInfo)
+                {
+                    textBox1.Text += $"Nombre: {subcarpeta.Name}\r\n";
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            FileInfo archivoGuardado = new FileInfo(@"C:\Users\baron\source\repos\UTN-PROG3\ArchivosDeUnaCarpeta\datos.txt");
+            using (FileStream fs = archivoGuardado.Open(FileMode.Create, FileAccess.ReadWrite))
+            {
+                using (StreamWriter writer = new StreamWriter(fs))
+                {
+                    writer.WriteLine(textBox1.Text);
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            FileInfo archivoGuardado = new FileInfo(@"C:\Users\baron\source\repos\UTN-PROG3\ArchivosDeUnaCarpeta\datos.txt");
+            string linea = null;
+            string datos = "";
+            using (FileStream fs = archivoGuardado.Open(FileMode.Open, FileAccess.ReadWrite))
+            {
+                using (StreamReader reader = new StreamReader(fs))
+                {
+                    while ((linea = reader.ReadLine()) != null)
+                    {
+                        datos += "\r\n"+linea;
+                    }
+                    textBox1.Text = datos;
+                }
+            }
+        }
+    }
+}
