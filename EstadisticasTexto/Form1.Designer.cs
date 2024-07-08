79ffacf87961f5e21185f4f0df30c42556742baa
ceso217 <baronicecilio@gmail.com>
2024-07-07 22:48:58 -0300

    Agregue ranking

--- EstadisticasTexto/Form1.Designer.cs
+++ EstadisticasTexto/Form1.Designer.cs
@@ -33,30 +33,44 @@
             this.label1 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.PosicionRanking = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Palabra = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.NroLetras = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.PorcentajeDelTexto = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.NroAparaciones = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(28, 54);
+            this.textBox1.Location = new System.Drawing.Point(21, 44);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(742, 343);
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(558, 279);
             this.textBox1.TabIndex = 0;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(324, 23);
+            this.label1.Location = new System.Drawing.Point(243, 19);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(128, 16);
+            this.label1.Size = new System.Drawing.Size(104, 13);
             this.label1.TabIndex = 1;
             this.label1.Text = "Palabras: 0 Letras: 0";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(623, 12);
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.button1.AutoSize = true;
+            this.button1.Location = new System.Drawing.Point(467, 10);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(135, 32);
+            this.button1.Size = new System.Drawing.Size(101, 26);
             this.button1.TabIndex = 2;
             this.button1.Text = "Elegir archivo";
             this.button1.UseVisualStyleBackColor = true;
@@ -67,16 +81,78 @@
             this.timer1.Enabled = true;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AccessibleName = "";
+            this.dataGridView1.AllowUserToOrderColumns = true;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.PosicionRanking,
+            this.Palabra,
+            this.NroLetras,
+            this.PorcentajeDelTexto,
+            this.NroAparaciones});
+            this.dataGridView1.Location = new System.Drawing.Point(21, 373);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(558, 286);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // PosicionRanking
+            // 
+            this.PosicionRanking.HeaderText = "Posición";
+            this.PosicionRanking.Name = "PosicionRanking";
+            this.PosicionRanking.ReadOnly = true;
+            // 
+            // Palabra
+            // 
+            this.Palabra.HeaderText = "Palabra";
+            this.Palabra.Name = "Palabra";
+            this.Palabra.ReadOnly = true;
+            // 
+            // NroLetras
+            // 
+            this.NroLetras.HeaderText = "Nro de Letras";
+            this.NroLetras.Name = "NroLetras";
+            this.NroLetras.ReadOnly = true;
+            // 
+            // PorcentajeDelTexto
+            // 
+            this.PorcentajeDelTexto.HeaderText = "% del Texto";
+            this.PorcentajeDelTexto.Name = "PorcentajeDelTexto";
+            this.PorcentajeDelTexto.ReadOnly = true;
+            // 
+            // NroAparaciones
+            // 
+            this.NroAparaciones.HeaderText = "Nro de Apariciones";
+            this.NroAparaciones.Name = "NroAparaciones";
+            this.NroAparaciones.ReadOnly = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(26, 341);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Ranking";
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(604, 671);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox1);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -88,6 +164,13 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn PosicionRanking;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Palabra;
+        private System.Windows.Forms.DataGridViewTextBoxColumn NroLetras;
+        private System.Windows.Forms.DataGridViewTextBoxColumn PorcentajeDelTexto;
+        private System.Windows.Forms.DataGridViewTextBoxColumn NroAparaciones;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
 
--- EstadisticasTexto/Form1.cs
+++ EstadisticasTexto/Form1.cs
@@ -31,28 +31,113 @@ namespace EstadisticasTexto
         }
 
         // conteo de palabras y letras
+        List<string> palabrasCantidad = new List<string>();
+        bool unaVez = true;
         private void timer1_Tick(object sender, EventArgs e)
         {
             int letras = 0;
-            string[] palabras = textBox1.Text.Split(new char[] { ',', ' ', '.' },StringSplitOptions.RemoveEmptyEntries);
+            palabrasCantidad = textBox1.Text.Split(new char[] { ',', ' ', '.' }, StringSplitOptions.RemoveEmptyEntries).ToList();
 
-            foreach(char c in textBox1.Text)
+            foreach (char c in textBox1.Text)
             {
-                if(c != ' ')
+                if (c != ' ' && c != ',' && c != '.')
                 {
                     letras++;
                 }
             }
 
-            label1.Text = $"Palabras: {palabras.Length} Letras: {letras}";
+            label1.Text = $"Palabras: {palabrasCantidad.Count} Letras: {letras}";
+
+            if (unaVez && letras > 0)
+            {
+                contarPalabras();
+                armarRanking();
+                unaVez = false;
+            }
         }
 
+        // contar repeticiones de palabras
+        List<Palabra> palabrasApariciones = new List<Palabra>();
+        public void contarPalabras()
+        {
+            bool esta;
+            int apar;
+            foreach (String p in palabrasCantidad)
+            {
+                esta = false;
+                apar = 0;
+                foreach (Palabra pal in palabrasApariciones)
+                {
+                    if (p == pal.Word)
+                    {
+                        esta = true;
+                    }
+                }
+                if (!esta)
+                {
+                    foreach (String s in palabrasCantidad)
+                    {
+                        if (p == s)
+                        {
+                            apar++;
+                        }
+                    }
+                    palabrasApariciones.Add(new Palabra(p, apar));
+                }
+            }
+        }
 
+        //armar Ranking
+        List<Palabra> ranking = new List<Palabra>();
+        public void ordenarRanking()
+        {
+            float mayor = 1;
+            int index = 0;
+            bool esta;
+            for (int i = 0; i < 10; i++)
+            {
+                foreach (Palabra p in palabrasApariciones)
+                {
+                    esta = false;
+                    if (p.Apariciones > mayor)
+                    {
+                        foreach (Palabra r in ranking)
+                        {
+                            if (p.Word == r.Word)
+                            {
+                                esta = true;
+                            }
+                        }
+                        if (!esta)
+                        {
+                            mayor = p.Apariciones;
+                            index = palabrasApariciones.IndexOf(p);
+                        }
+                    }
+                }
+                ranking.Add(palabrasApariciones[index]);
+                mayor = 1;
+            }
+        }
+        public void armarRanking()
+        {
+            ordenarRanking();
+            for (int i = 0; i < 10; i++)
+            {
+                dataGridView1.Rows.Add(i + 1, ranking[i].Word, ranking[i].Word.Length, $"%{((ranking[i].Apariciones/palabrasCantidad.Count)*100):F1}", ranking[i].Apariciones);
+            }
+        }
+    }
+}
 
+class Palabra
+{
+    public string Word { get; set; }
+    public float Apariciones { get; set; }
 
-
-
-
-
+    public Palabra(string word, int apariciones)
+    {
+        this.Word = word;
+        this.Apariciones = apariciones;
     }
 }
