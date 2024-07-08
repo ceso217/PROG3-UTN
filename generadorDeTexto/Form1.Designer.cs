3ea2f6bd3abdff9a7fbaf7c3932f8b9963c3d952
ceso217 <baronicecilio@gmail.com>
2024-07-07 22:31:45 -0300

    Generador de texto tipo Lorem Ipsum

---
+++ generadorDeTexto/Form1.Designer.cs
@@ -0,0 +1,270 @@
+﻿namespace generadorDeTexto
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.numericUpDownParrafos = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.numericUpDownPalabras = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label7 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownParrafos)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownPalabras)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(11, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Parámetros";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(15, 82);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(330, 17);
+            this.checkBox1.TabIndex = 1;
+            this.checkBox1.Text = "Iniciar con Lorem ipsum dolor sit amet, consectetur adipiscing elit";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // numericUpDownParrafos
+            // 
+            this.numericUpDownParrafos.Location = new System.Drawing.Point(128, 45);
+            this.numericUpDownParrafos.Name = "numericUpDownParrafos";
+            this.numericUpDownParrafos.Size = new System.Drawing.Size(35, 20);
+            this.numericUpDownParrafos.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(14, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(108, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Cantidad de parrafos:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(169, 47);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(131, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "o     Cantidad de palabras:";
+            // 
+            // numericUpDownPalabras
+            // 
+            this.numericUpDownPalabras.Increment = new decimal(new int[] {
+            25,
+            0,
+            0,
+            0});
+            this.numericUpDownPalabras.Location = new System.Drawing.Point(306, 45);
+            this.numericUpDownPalabras.Name = "numericUpDownPalabras";
+            this.numericUpDownPalabras.Size = new System.Drawing.Size(35, 20);
+            this.numericUpDownPalabras.TabIndex = 5;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(238, 124);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(35, 20);
+            this.numericUpDown1.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 150);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(222, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Cantidad máxima de palabras en una oración:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(13, 126);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(221, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Cantidad mínima de palabras en una oración:";
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(238, 148);
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(35, 20);
+            this.numericUpDown2.TabIndex = 6;
+            // 
+            // numericUpDown3
+            // 
+            this.numericUpDown3.Location = new System.Drawing.Point(238, 183);
+            this.numericUpDown3.Name = "numericUpDown3";
+            this.numericUpDown3.Size = new System.Drawing.Size(35, 20);
+            this.numericUpDown3.TabIndex = 11;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 185);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(220, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Cantidad máxima de oraciones en un párrafo:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(15, 217);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(161, 23);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Generar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(103, 596);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(161, 23);
+            this.button2.TabIndex = 14;
+            this.button2.Text = "Guardar texto";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(412, 596);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(161, 23);
+            this.button3.TabIndex = 15;
+            this.button3.Text = "Cargar texto";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(509, 227);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(113, 13);
+            this.label7.TabIndex = 17;
+            this.label7.Text = "Cantidad de palabras: ";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 246);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(696, 344);
+            this.richTextBox1.TabIndex = 18;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(723, 621);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.numericUpDown3);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.numericUpDownPalabras);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.numericUpDownParrafos);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Generador de texto aleatorio";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownParrafos)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownPalabras)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.NumericUpDown numericUpDownParrafos;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown numericUpDownPalabras;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.NumericUpDown numericUpDown3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ generadorDeTexto/Form1.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace generadorDeTexto
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string[] palabras = File.ReadAllText(@"C:\Users\baron\Desktop\textolatin.txt").Split(new char[] { ',', '.', ' ', ':', '?', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+
+        // generar texto
+
+        Random rnd = new Random();
+        bool lorem = false;
+        int cantidadParrafos;
+        int cantidadPalabrasTexto;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            int cantidadPalabrasOracion;
+
+            int cantidadOracionesParrafo;
+
+            string texto = "";
+
+            bool primera = true;
+
+            if (lorem)
+            {
+                texto = "Lorem ipsum dolor sit amet, consectetur adipiscing elit.";
+            }
+            // si selecciono cantidad de palabras
+            if (numericUpDownPalabras.Enabled)
+            {
+                // si selecciona cantidad de palabras
+                while (cantidadPalabrasTexto > 0)
+                {
+                    // armado de parrafos (oraciones por parrafo)
+                    cantidadOracionesParrafo = rnd.Next(1, Convert.ToInt32(numericUpDown3.Value + 1));
+                    for (int i = 0; i < cantidadOracionesParrafo; i++)
+                    {
+                        // armado de oraciones (palabras por oracion)
+                        if (cantidadPalabrasTexto > Convert.ToInt32(numericUpDown1.Value) + Convert.ToInt32(numericUpDown2.Value + 1))
+                        {
+                            cantidadPalabrasOracion = rnd.Next(Convert.ToInt32(numericUpDown1.Value), Convert.ToInt32(numericUpDown2.Value + 1));
+                        }
+                        else
+                        {
+                            if (cantidadPalabrasTexto < Convert.ToInt32(numericUpDown2.Value))
+                            {
+                                cantidadPalabrasOracion = cantidadPalabrasTexto;
+                            }
+                            else
+                            {
+                                cantidadPalabrasOracion = Convert.ToInt32(numericUpDown1.Value);
+                            }
+                        }
+                        for (int j = 0; j < cantidadPalabrasOracion; j++)
+                        {
+                            if (cantidadPalabrasTexto > 0)
+                            {
+                                string palabra = palabras[rnd.Next(0, palabras.Length)];
+                                if (primera)
+                                {
+                                    palabra = char.ToUpper(palabra[0]) + palabra.Substring(1);
+                                    primera = false;
+                                }
+                                texto += " " + palabra;
+
+                                cantidadPalabrasTexto--;
+                            }
+                        }
+                        texto += ". ";
+                        primera = true;
+                        if (cantidadPalabrasTexto == 0)
+                        {
+                            break;
+                        }
+                    }
+                    texto += "(SALTO)\n\n";
+                }
+            }
+
+            // si selecciono cantidad de parrafos
+            if (numericUpDownParrafos.Enabled)
+            {
+                // si selecciona cantidad de palabras
+                while (cantidadParrafos > 0)
+                {
+                    // armado de parrafos (oraciones por parrafo)
+                    cantidadOracionesParrafo = rnd.Next(1, Convert.ToInt32(numericUpDown3.Value + 1));
+                    for (int i = 0; i < cantidadOracionesParrafo; i++)
+                    {
+                        // armado de oraciones (palabras por oracion)
+                        cantidadPalabrasOracion = rnd.Next(Convert.ToInt32(numericUpDown1.Value), Convert.ToInt32(numericUpDown2.Value + 1));
+                        for (int j = 0; j < cantidadPalabrasOracion; j++)
+                        {
+                            string palabra = palabras[rnd.Next(0, palabras.Length)];
+                            if (primera)
+                            {
+                                palabra = char.ToUpper(palabra[0]) + palabra.Substring(1);
+                                primera = false;
+                            }
+                            texto += " " + palabra;
+                        }
+                        texto += ". ";
+                        primera = true;
+                    }
+                    texto += "(SALTO)\n\n";
+                    cantidadParrafos--;
+                }
+            }
+
+            richTextBox1.Text = texto;
+            string[] cantPalabras = texto.Split(new char[] { '\n', '.', ' ', '*' }, StringSplitOptions.RemoveEmptyEntries);
+            label7.Text = $"Cantidad de palabras: {cantPalabras.Length}";
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            for (int i = 0; i < palabras.Length; i++)
+            {
+                palabras[i] = palabras[i].ToLower();
+            }
+
+            if (numericUpDownParrafos.Value != 0)
+            {
+                cantidadParrafos = (int)numericUpDownParrafos.Value;
+                numericUpDownPalabras.Enabled = false;
+            }
+            else
+            {
+                cantidadParrafos = rnd.Next(1, 6);
+                numericUpDownPalabras.Enabled = true;
+            }
+
+            if (numericUpDownPalabras.Value != 0)
+            {
+                cantidadPalabrasTexto = (int)numericUpDownPalabras.Value;
+                numericUpDownParrafos.Enabled = false;
+            }
+            else
+            {
+                cantidadPalabrasTexto = rnd.Next(60, 301);
+                numericUpDownParrafos.Enabled = true;
+            }
+
+            if (checkBox1.Checked)
+            {
+                lorem = true;
+                cantidadPalabrasTexto -= 8;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            File.WriteAllText("textoAutogenerado.txt", richTextBox1.Text);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = File.ReadAllText("textoAutogenerado.txt");
+        }
+    }
+}
